97b9a8094abefd9b704002462bfa9fa5961ed019
Olivier Robardet <olivier.robardet@gmail.com>
2016-02-13 19:08:47 +0100

    Setup : formulaire ajout utilisateur
    Mise en place d'un formulaire pour l'ajout du premier utilisateur dans le setup.
    Le formulaire est fonctionnel, ainsi que le message de réussite final et la gestion des erreurs de saisies.
    Reste juste l'enregistrement des données de l'utilisateur saisi dans la BDD.

--- modules/setup/module.js
+++ modules/setup/module.js
@@ -1,13 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var form = require('express-form');
+var field = form.field;
 
 /* add user */
-router.post('/', function (req, res, next) {
-    if ((req.body.password !== req.body.password2) ||
-        (req.body.password === '') ||
-        (req.body.password2 === '')) {
-        next('route');
+router.post('/', form(
+    field("email", "Email")
+        .trim()
+        .required()
+        .isEmail(),
+    field("firstname", "Firstname")
+        .trim()
+        .required()
+        .is(/^[a-zA-Z -']+$/),
+    field("lastname", "Lastname")
+        .trim()
+        .is(/^[a-zA-Z -']+$/),
+    field("password", "Password")
+        .trim()
+        .required()
+        .is(/^\w{6,}$/),
+    field("password2", "Password verification")
+        .trim()
+        .required()
+        .equals("field::password", "Passwords does not match")
+), function (req, res, next) {
+    if (!req.form.isValid) {
+        console.log(req.form.getErrors());
+        res.render('setup/index', {
+            layout: 'setup/layout',
+            errors: req.form.getErrors()
+        });
     } else {
+        // TODO: save user
         res.redirect("/setup/done");
     }
 });
@@ -15,7 +40,11 @@ router.post('/', function (req, res, next) {
 /* setup root */
 router.get('/', function (req, res) {
     res.render('setup/index', {
-        layout: 'setup/layout'
+        layout: 'setup/layout',
+        errors: [],
+        email: null,
+        firstname: null,
+        lastname: null
     });
 });
 
