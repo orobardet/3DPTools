f1a35fd670f61840ff39de426f3547ec6b6272a9
Olivier Robardet <olivier.robardet@gmail.com>
2016-02-27 00:48:00 +0100

    Liste des magasins

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,19 @@
 module.exports = function (app) {
+    var when = require('when');
+    var Shop = app.models.shop;
 
-    this.index = function (req, res) {
-        res.render('shop/index');
+    this.index = function (req, res, next) {
+        when(Shop.find().sort('name').exec())
+            .then(function (shops) {
+                return res.render('shop/index', {
+                    shops: shops,
+                    pageTitle: 'Shops',
+                    errors: []
+                });
+            })
+            .catch(function (err) {
+                return next(err);
+            });
     };
 
     return this;
