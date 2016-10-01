213385ca6ff6898a419cf9c2391c9bbe79d46ccc
Olivier Robardet <olivier.robardet@gmail.com>
2016-10-01 19:12:26 +0200

    Filament: v4 model migrator

---
+++ src/models/migrate/filament/v4.js
@@ -0,0 +1,24 @@
+module.exports = function(filament, currentVersion) {
+    var localVersion = 4;
+
+    this.needMigration = function() {
+        if (!filament._version || filament._version < localVersion) {
+            return true;
+        }
+
+        return false;
+    };
+
+    this.migrate = function() {
+        if (!this.needMigration()) {
+            return;
+        }
+
+        filament.finished = false;
+        filament.finishedDate = null;
+
+        filament._version = localVersion;
+    }
+
+    return this;
+};
