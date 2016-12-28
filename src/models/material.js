89fa20ecfedb2b5df2cbe04a53043b3306b394ce
Olivier Robardet <olivier.robardet@gmail.com>
2016-12-28 23:32:41 +0100

    Ajout versioning et migration model material
    Fix #42

--- src/models/material.js
+++ src/models/material.js
@@ -14,8 +14,66 @@ module.exports = function (app) {
         bedTemp: {
             min: Number,
             max: Number
-        }
+        },
+        _version : { type: Number }
     });
+
+    /**
+     * Current schema version
+     *
+     * @type {number}
+     *
+     * <b>Version history:</b>
+     * - 0: initial model
+     */
+    materialSchema.statics.currentVersion = 0;
+
+    materialSchema.methods.setInMigration = function() {
+        this.inMigration = true;
+    }
+
+    materialSchema.methods.isInMigration = function() {
+        return this.inMigration;
+    }
+
+    materialSchema.methods.resetInMigration = function() {
+        delete this.inMigration;
+    }
+
+    materialSchema.pre('save', function(next) {
+        // No pre save during migration, to avoid updating unwanted data
+        if (this.isInMigration()) {
+            this.resetInMigration();
+            next();
+            return;
+        }
+
+        this._version = materialSchema.statics.currentVersion;
+
+        next();
+    });
+
+    materialSchema.methods.migrate = function() {
+        var that = this;
+
+        var migrated = false;
+        if (app.models.migrate && app.models.migrate.filament) {
+            Object.keys(app.models.migrate.filament).forEach(function (element, index) {
+                var migrator = new app.models.migrate.filament[element](that, materialSchema.statics.currentVersion);
+
+                if (migrator.needMigration && typeof migrator.needMigration === 'function' && migrator.needMigration()) {
+                    migrated = true;
+                    if (migrator.migrate && typeof migrator.migrate === 'function') {
+                        migrator.migrate();
+                    }
+                }
+            });
+        }
+
+        this._version = materialSchema.statics.currentVersion;
+
+        return migrated;
+    };
 
     materialSchema.statics.findById = function (id, cb) {
         return this.findOne({_id: id}, cb);
